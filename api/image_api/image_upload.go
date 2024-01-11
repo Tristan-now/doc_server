d2ce381f98644d7c4bbd6fd1225705defa6239c4
arco_notebook <3481554467@qq.com>
2024-01-11 17:00:37 +0800

    image_upload_whitelist

--- api/image_api/image_upload.go
+++ api/image_api/image_upload.go
@@ -5,7 +5,17 @@ import (
 	"gvd_server/service/common/res"
 	"gvd_server/utils/jwts"
 	"path"
+	"strings"
 )
+
+var ImageWhiteList = []string{
+	"jpg",
+	"png",
+	"jpeg",
+	"gif",
+	"svg",
+	"webp",
+}
 
 // ImageUploadView 上传图片
 // @Tags 图片管理
@@ -26,8 +36,29 @@ func (ImageApi) ImageUploadView(c *gin.Context) {
 	_claims, _ := c.Get("claims")
 	claims, _ := _claims.(*jwts.CustomClaims)
 
+	if !InImageWhiteList(file.Filename, ImageWhiteList) {
+		res.FailWithMsg("文件非法", c)
+		//log.Warn("文件非法")
+		return
+	}
+
 	path := path.Join("uploads", claims.NickName, file.Filename)
 	c.SaveUploadedFile(file, path)
 	res.OKWithData("图片上传成功", c)
 
+}
+
+func InImageWhiteList(fileName string, whiteList []string) bool {
+	// 截取文件后缀
+	_list := strings.Split(fileName, ".") // xxx  1.2 xxx.png xxx.PNG  xxx.png   xxx.1.2.png  xxxx.png.exe
+	if len(_list) < 2 {
+		return false
+	}
+	suffix := strings.ToLower(_list[len(_list)-1])
+	for _, s := range whiteList {
+		if suffix == s {
+			return true
+		}
+	}
+	return false
 }
